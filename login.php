e8863dceb4d641b2c233ee6f95dbffbea0ff05a5
DCSchram <dcschram@svsu.edu>
2025-03-24 10:51:28 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,23 @@
 <?php
-
-
-
-
-
-
+session_start();
 
 ?>
+
+<!DOCTYPE html>
+<html lang=en>
+<head>
+  <title>Issues List Report</title>
+  <meta charset=utf-8>
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+  <main>
+
+
+
+
+  
+  </main>
+</body>
+</html>
